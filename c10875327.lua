c19e44da4bf2e86c689c97c642066cbbad5dfc2c
Vury Leo <i@vuryleo.com>
2025-06-30 22:00:44 +0900

    fix 地縛神 Aslla piscu

--- c10875327.lua
+++ c10875327.lua
@@ -1,5 +1,6 @@
 --地縛神 Aslla piscu
-function c10875327.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	c:SetUniqueOnField(1,1,aux.FilterBoolFunction(Card.IsSetCard,0x1021),LOCATION_MZONE)
 	--
 	local e4=Effect.CreateEffect(c)
@@ -7,7 +8,7 @@ function c10875327.initial_effect(c)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCode(EFFECT_SELF_DESTROY)
-	e4:SetCondition(c10875327.sdcon)
+	e4:SetCondition(s.sdcon)
 	c:RegisterEffect(e4)
 	--battle target
 	local e5=Effect.CreateEffect(c)
@@ -24,36 +25,46 @@ function c10875327.initial_effect(c)
 	c:RegisterEffect(e6)
 	--destroy
 	local e7=Effect.CreateEffect(c)
-	e7:SetDescription(aux.Stringid(10875327,0))
+	e7:SetDescription(aux.Stringid(id,0))
 	e7:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e7:SetCode(EVENT_LEAVE_FIELD)
-	e7:SetCondition(c10875327.descon)
-	e7:SetTarget(c10875327.destg)
-	e7:SetOperation(c10875327.desop)
+	e7:SetCondition(s.descon)
+	e7:SetTarget(s.destg)
+	e7:SetOperation(s.desop)
 	c:RegisterEffect(e7)
 end
-function c10875327.sdcon(e)
+function s.sdcon(e)
 	return not Duel.IsExistingMatchingCard(Card.IsFaceup,0,LOCATION_FZONE,LOCATION_FZONE,1,nil)
 end
-function c10875327.descon(e,tp,eg,ep,ev,re,r,rp)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousPosition(POS_FACEUP) and not c:IsLocation(LOCATION_DECK)
-		and (not re or re:GetOwner()~=c)
+	if not c:IsPreviousPosition(POS_FACEUP) then
+		return false
+	end
+	if c:IsLocation(LOCATION_DECK) then
+		return false
+	end
+	--- Hack for if destroyed by rule (get control by no slot, lost equip target etc)
+	--- remove this hack once the reason effect does not change issue is addressed in core
+	if (r&REASON_RULE~=0) then
+		return true
+	end
+	return not re or re:GetOwner()~=c
 end
-function c10875327.desfilter(c)
+function s.desfilter(c)
 	return c:IsFaceup()
 end
-function c10875327.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(c10875327.desfilter,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 	if g:GetCount()~=0 then
 		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetCount()*800)
 	end
 end
-function c10875327.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c10875327.desfilter,tp,0,LOCATION_MZONE,nil)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
 	local ct=Duel.Destroy(g,REASON_EFFECT)
 	if ct~=0 then
 		Duel.Damage(1-tp,ct*800,REASON_EFFECT)
